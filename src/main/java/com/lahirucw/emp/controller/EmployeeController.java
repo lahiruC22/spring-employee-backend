3deb6d8678fcd5fb718e3e3d9e30d9e2b376c8d3
lahirucweducatiuon <lahirucweducatiuon@gmail.com>
2025-04-21 18:55:41 +0000

    #003 fix the conflicts of the repo

--- src/main/java/com/lahirucw/emp/controller/EmployeeController.java
+++ src/main/java/com/lahirucw/emp/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,6 @@ import com.lahirucw.emp.mapper.EmployeeMapper;
 import com.lahirucw.emp.model.Employee;
 import com.lahirucw.emp.service.EmployeeService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("api/employees")
